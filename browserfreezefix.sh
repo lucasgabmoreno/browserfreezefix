d02089c6d1863707cf41a44094105e5cb2d00aa8
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2023-01-24 15:24:19 -0300

    Add files via upload

---
+++ browserfreezefix.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Copy this file into /usr/bin/ folder
+# sudo chmod +x /usr/bin/browserfreeze-fix.sh
+# Add /usr/bin/browserfreeze-fix.sh to startup
+
+rmcache() {
+rm -rf $HOME/.cache/"$1" 2> /dev/null
+}
+
+rmcache BraveSoftware/Brave-Browser # Brave
+rmcache chromium # Chromium
+rmcache epiphany # Epiphany
+rmcache falkon # Falkon 
+rmcache google-chrome # Google Chrome
+rmcache konqueror # Konqueror
+rmcache opera # Opera
+rmcache Microsoft/Edge # Microsoft Edge
+rmcache microsoft-edge # Microsoft Edge
+rmcache mozilla/firefox # Mozilla Firefox
+rmcache netsurf # NetSurf
+rmcache qutebrowser # Qutebrowser
+rmcache torbrowser # Tor Browser
+rmcache vivaldi # Vivaldi
+rmcache yandex-browser # Yandex browser
+
+# No information about cache dir:
+# Dillo (no cache dir)
+# Links
+# Lynx
+# Surf
+# SRWare Iron
+# w3m
+
+# Can't install:
+# Firedragon
+# GNUzilla
+# Iceweasel
+# Icecat
+# LibreWolf
+# Midori
+# Palemoon
+# QupZilla (actual Falkon)
